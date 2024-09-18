3b8b54f3985b90b4eec409e988182840a095d161
iharsh02 <ht266005@gmail.com>
2024-09-18 23:04:25 +0530

    add path pattern in action.json

--- app/actions.json/route.ts
+++ app/actions.json/route.ts
@@ -5,7 +5,7 @@ export const GET = async () => {
     rules: [
       // map all root level routes to an action
       {
-        pathPattern: "/",
+        pathPattern: "/**",
         apiPath: "/api/donate/",
       },
     ],
