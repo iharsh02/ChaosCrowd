b8a604392bdac6a4bbf4c4613cd60ef964562577
iharsh02 <ht266005@gmail.com>
2024-09-18 20:04:47 +0530

    fixed the ui overflow

--- app/discover/page.tsx
+++ app/discover/page.tsx
@@ -5,17 +5,16 @@ import { Profile } from "@/components/Discover/Profile";
 export default function DiscoverPage() {
   return (
     <div>
-
       <section className=" lg:w-1/2 m-auto">
         <Profile />
         <div>
           <Bar />
         </div>
       </section>
-      
-        <section className="lg:w-1/2 m-auto mb-5">
-          <UserFetch />
-        </section>
+
+      <section className="lg:w-1/2 m-auto mb-5">
+        <UserFetch />
+      </section>
     </div>
   );
 }
